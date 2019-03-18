b4b65cabfcff7af5ed882128722fa29e9fb5f640
Ayan Shukatayev <shukatayev@yahoo.com>
2019-03-18 15:25:07 +0000

    Adding a new person - Michael Jordan

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -7,6 +7,9 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        else if (query.toLowerCase().contains("jordan")) {
+            return "Michael Jordan, Basketball player";
+        }
         return "";
     }
 }
